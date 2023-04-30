a89aad64f8786dc04d3c518d55ff246e4fcd3b35
Prieance <p.sagar@saedubai.com>
2023-04-30 18:36:54 +0400

    Main Menu
    -Added script for main menu load

---
+++ Horror Mansion/Assets/Scripts/Menu.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class Menu : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public void PlayGame()
+    {
+        SceneManager.LoadScene("Main");
+    }
+
+    public void ExitGame()
+    {
+        Application.Quit();
+    }
+}
